424e28ea161d88b3ecdbf1c18bc91b8173b2b837
Louis Larpin <louis.larpin@gmail.com>
2015-12-18 15:47:50 +0100

    fix test

--- spec/features/home_page_message_spec.rb
+++ spec/features/home_page_message_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 describe "the home page displayed message" do
   it "says hello" do
     visit "/"
-    expect(page).to have_content("Hello World !")
+    expect(page).to have_content("Hello Supinfo !")
   end
 end
